12667ee4e2ef54c24b3f303b91553d905b8bb18b
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-06-17 15:05:56 +0700

    return token when loged in

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import { getProjects } from "../controllers/projects.js";
+import { createProject, getProjects } from "../controllers/projects.js";
 
 router.get("/", getProjects);
+router.post("/", createProject);
 
 export default router;
