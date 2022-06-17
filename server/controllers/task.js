cc0404d6fb9746540effe473f0f665407ee3bf59
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-06-17 16:13:15 +0700

    create delete/update task api

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -3,7 +3,7 @@ import Task from "../models/Task.js";
 
 export const getTaskById = async (req, res) => {
   const { projectId } = req.params;
-  console.log(projectId)
+  console.log(projectId);
   try {
     const data = await Task.find({ projectId: projectId });
     res.status(200).json(data);
@@ -22,21 +22,20 @@ export const createTask = async (req, res) => {
   }
 };
 
-export const deleteProject = async (req, res) => {
+export const deleteTask = async (req, res) => {
   const { id } = req.params;
   const isIdExist = mongoose.Types.ObjectId.isValid(id);
-  if (!isIdExist) return res.status(404).json({ message: "project not found" });
-  await Project.deleteOne({ _id: id });
-  res.status(200).json({ message: "project is deleted" });
+  if (!isIdExist) return res.status(404).json({ message: "task not found" });
+  await Task.deleteOne({ _id: id });
+  res.status(200).json({ message: "task is deleted" });
 };
 
-export const updateProject = async (req, res) => {
+export const updateTask = async (req, res) => {
   const { id } = req.params;
-  const { projectName, projectDescription, owner, members } = req.body;
   const isIdExist = mongoose.Types.ObjectId.isValid(id);
   if (!isIdExist) return res.status(404).json({ message: "project not found" });
 
-  const updatedProject = { projectName, projectDescription, owner, members, _id: id };
-  await Project.findByIdAndUpdate(id, updatedProject, { new: true });
-  res.status(200).json({ message: "project is updated successfully" });
+  const updatedTask = { ...req.body, _id: id };
+  await Task.findByIdAndUpdate(id, updatedTask, { new: true });
+  res.status(200).json({ message: "task is updated successfully" });
 };
--- server/routes/task.js
+++ server/routes/task.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { createTask, getTaskById } from "../controllers/task.js";
+import { createTask, deleteTask, getTaskById, updateTask } from "../controllers/task.js";
 
 router.get("/:projectId", getTaskById);
-router.post("/", createTask)
+router.post("/", createTask);
+router.delete("/:id", deleteTask);
+router.patch("/:id", updateTask);
 
 export default router;
