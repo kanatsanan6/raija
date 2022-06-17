22b3db279a32fb04c740108a2ac318a9cfdcb7ec
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-06-17 15:28:50 +0700

    create updated api

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -27,3 +27,14 @@ export const deleteProject = async (req, res) => {
   await Project.deleteOne({ _id: id });
   res.status(200).json({ message: "project is deleted" });
 };
+
+export const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const { projectName, projectDescription, owner, members } = req.body;
+  const isIdExist = mongoose.Types.ObjectId.isValid(id);
+  if (!isIdExist) return res.status(404).json({ message: "project not found" });
+
+  const updatedProject = { projectName, projectDescription, owner, members, _id: id };
+  await Project.findByIdAndUpdate(id, updatedProject, { new: true });
+  res.status(200).json({ message: "project is updated successfully" });
+};
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { createProject, deleteProject, getProjects } from "../controllers/projects.js";
+import { createProject, deleteProject, getProjects, updateProject } from "../controllers/projects.js";
 
 router.get("/", getProjects);
 router.post("/", createProject);
 router.delete("/:id", deleteProject);
+router.patch("/:id", updateProject);
 
 export default router;
