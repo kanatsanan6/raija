acb90d9638718c318185cd3c27d700cbf94bc024
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-06-17 15:18:28 +0700

    add project delete api

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Project from "../models/Project.js";
 
 export const getProjects = async (req, res) => {
@@ -17,4 +18,12 @@ export const createProject = async (req, res) => {
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
+};
+
+export const deleteProject = async (req, res) => {
+  const { id } = req.params;
+  const isIdExist = mongoose.Types.ObjectId.isValid(id);
+  if (!isIdExist) return res.status(404).json({ message: "project not found" });
+  await Project.deleteOne({ _id: id });
+  res.status(200).json({ message: "project is deleted" });
 };
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { createProject, getProjects } from "../controllers/projects.js";
+import { createProject, deleteProject, getProjects } from "../controllers/projects.js";
 
 router.get("/", getProjects);
 router.post("/", createProject);
+router.delete("/:id", deleteProject);
 
 export default router;
