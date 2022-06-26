a2222817e21cd5c6514d14417390cb60968b0b56
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-06-26 15:20:38 +0700

    fix sidebar animation

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -15,14 +15,16 @@ function Sidebar({ setIsShowSidebar }) {
 
   return (
     <div className="fixed top-0 flex h-screen w-screen">
-      <div className={`${sidebarAnimationStyle}  h-[100%] w-[280px] bg-white py-6`}>
-        <div className="flex w-[10%] items-center px-5">
-          <h4 className="text-xl font-semibold leading-7 tracking-wider text-[#253858]">RAIJA</h4>
+      <div className="h-[100%] w-[280px] bg-white py-6">
+        <div className={`${sidebarAnimationStyle}`}>
+          <div className="flex w-[10%] items-center px-5">
+            <h4 className="text-xl font-semibold leading-7 tracking-wider text-[#253858]">RAIJA</h4>
+          </div>
+          <hr className="mx-5 mt-3" />
+          <p className="mt-1 mb-3 px-5 text-sm text-gray-500">Page</p>
+          <SidebarItem itemName="Dashboard" itemLogo="/images/dashboard-logo.svg" pathName="/" />
+          <SidebarItem itemName="Profile" itemLogo="/images/profile-logo.svg" pathName="/profile" />
         </div>
-        <hr className="mx-5 mt-3" />
-        <p className="mt-1 mb-3 px-5 text-sm text-gray-500">Page</p>
-        <SidebarItem itemName="Dashboard" itemLogo="/images/dashboard-logo.svg" pathName="/" />
-        <SidebarItem itemName="Profile" itemLogo="/images/profile-logo.svg" pathName="/profile" />
       </div>
       <div className={`${bgAnimationStyle} appearIn flex-1 bg-black bg-opacity-30`} onClick={closeSidebar} />
     </div>
