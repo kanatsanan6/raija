69f02a02a41331a286cf1aafa000033fab5f1c8e
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-07-05 15:23:46 +0700

    fix member show in ProjectCard

--- client/src/components/ProjectCard/ProjectCard.jsx
+++ client/src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,13 @@
 import { Avatar, AvatarGroup } from "@mui/material";
+import { useStateValue } from "../../context/StateProvider";
 
 function ProjectCard({ project }) {
+  const [{ users }] = useStateValue();
   const projectName = project.projectName;
   const projectDescription = project.projectDescription;
-  const projectMember = project.members;
+  const projectMember = users.users.filter((user) => project.members.includes(user._id));
+  console.log(project.members);
+  console.log(projectMember);
 
   return (
     <div className="flex h-40 w-[100%] flex-col justify-between rounded-lg bg-white p-5">
@@ -11,7 +15,7 @@ function ProjectCard({ project }) {
       <h4 className="mb-2 text-sm text-[#3D3D3D] line-clamp-3">{projectDescription}</h4>
       <AvatarGroup sx={{ transform: "scale(0.8)", marginRight: "-20px" }} max={4}>
         {projectMember.map((member, index) => {
-          return <Avatar key={index}>{member[0].toUpperCase()}</Avatar>;
+          return <Avatar key={index}>{member.name[0].toUpperCase()}</Avatar>;
         })}
       </AvatarGroup>
     </div>
