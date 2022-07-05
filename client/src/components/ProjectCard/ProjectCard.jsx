f4db3d5fc0e57a8cf5937c187dd4598d17cfd522
kanatsanan6 <76254274+kanatsanan6@users.noreply.github.com>
2022-07-05 20:06:11 +0700

    fix font size

--- client/src/components/ProjectCard/ProjectCard.jsx
+++ client/src/components/ProjectCard/ProjectCard.jsx
@@ -10,8 +10,8 @@ function ProjectCard({ project }) {
 
   return (
     <div className="flex h-40 w-[100%] flex-col justify-between rounded-lg bg-white p-5">
-      <h3 className="text-xl font-semibold line-clamp-1">{projectName}</h3>
-      <h4 className="mb-2 text-sm text-[#3D3D3D] line-clamp-3">{projectDescription}</h4>
+      <h3 className="text-lg font-semibold line-clamp-1 sm:text-xl">{projectName}</h3>
+      <h4 className="mb-2 text-xs text-[#3D3D3D] line-clamp-3 sm:text-sm">{projectDescription}</h4>
       <AvatarGroup sx={{ transform: "scale(0.8)", marginRight: "-20px" }} max={4}>
         {projectMember.map((member, index) => {
           return (
